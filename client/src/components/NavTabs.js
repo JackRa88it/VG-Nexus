39e3b79045d880d1172ee78bbef3b7f254a56e2e
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2018-10-05 13:59:28 -0700

    move login/signup to right side nav

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -22,96 +22,98 @@ class NavTabs extends Component{
   render(){
     return(
       // nav-tabs nav-item
-    <ul className="nav nav-tabs"> 
-      <li className="nav-item">
-      <Link
-          to="/"
-        >
-          <img src={logo} alt="logo" id="logo"/>
-        </Link>
+    <nav className="navbar navbar-expand-lg w-100">
+      <ul className="nav nav-tabs navbar-collapse"> 
+        <li className="nav-item">
+         <Link to="/" >
+            <img src={logo} alt="logo" id="logo"/>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/"
+            className={
+              window.location.pathname === "/" ? "nav-link active" : "nav-link"
+            }
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/about"
+            className={
+              window.location.pathname === "/about" ? "nav-link active" : "nav-link"
+            }
+          >
+            About
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/all"
+            className={
+              window.location.pathname === "/all"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Choose Game
+          </Link>
       </li>
       <li className="nav-item">
-        <Link
-          to="/"
-          className={
-            window.location.pathname === "/" ? "nav-link active" : "nav-link"
-          }
-        >
-          Home
-        </Link>
+          <Link
+            to="/upload"
+            className={
+              window.location.pathname === "/upload"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Upload
+          </Link>
+      </li>
+      </ul>
+      <ul className="nav nav-tabs">
+      <li className="nav-item">
+        {!this.state.authenticated ? (
+          <Link
+            to="/login"
+            className={
+              window.location.pathname === "/login"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Login
+          </Link>) : (
+          <Link
+            to="/logout"
+            className={
+              window.location.pathname === "/logout"
+                ? "nav-link active"
+                : "nav-link"
+            }
+            onClick = {this.logoutHandler}
+          >
+            Logout
+          </Link>  
+          )}
       </li>
       <li className="nav-item">
-        <Link
-          to="/about"
-          className={
-            window.location.pathname === "/about" ? "nav-link active" : "nav-link"
-          }
-        >
-          About
-        </Link>
+          <Link
+            to="/signup"
+            className={
+              window.location.pathname === "/signup"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Sign Up
+          </Link>
       </li>
-      <li className="nav-item">
-        <Link
-          to="/all"
-          className={
-            window.location.pathname === "/all"
-              ? "nav-link active"
-              : "nav-link"
-          }
-        >
-          Choose Game
-        </Link>
-    </li>
-    <li className="nav-item">
-        <Link
-          to="/upload"
-          className={
-            window.location.pathname === "/upload"
-              ? "nav-link active"
-              : "nav-link"
-          }
-        >
-          Upload
-        </Link>
-    </li>
-    <li className="nav-item">
-      {!this.state.authenticated ? (
-        <Link
-          to="/login"
-          className={
-            window.location.pathname === "/login"
-              ? "nav-link active"
-              : "nav-link"
-          }
-        >
-          Login
-        </Link>) : (
-        <Link
-          to="/logout"
-          className={
-            window.location.pathname === "/logout"
-              ? "nav-link active"
-              : "nav-link"
-          }
-          onClick = {this.logoutHandler}
-        >
-          Logout
-        </Link>  
-        )}
-    </li>
-    <li className="nav-item">
-        <Link
-          to="/signup"
-          className={
-            window.location.pathname === "/signup"
-              ? "nav-link active"
-              : "nav-link"
-          }
-        >
-          Sign Up
-        </Link>
-    </li>
-    </ul>
+      </ul>
+    </nav>
   )};
 }
   
