f721b58a6faa4c0f398c7b7c2b1b73bcfd6efc07
Hoff <haffedmcnair@gmail.com>
2018-10-12 15:56:48 -0700

    favorites display on homepage

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -167,6 +167,23 @@ module.exports = function (app,io){
             res.json(err)
         })
     })
+
+    app.get('/api/user/favorites', (req,res)=>{
+        db.User.findOne({
+            where: {id: req.user.id},
+        }).then((user)=>{
+            user.getFavorites()
+            .then(function(fav){
+                res.json(fav)
+            })
+            // res.json(user)
+        }).catch(function(err){
+            console.log(err);
+            res.json(err)
+        })
+    })
+
+
     app.get('/api/games/newest', function(req,res){
         db.Game.findAll({
             limit: 8,
@@ -278,13 +295,10 @@ module.exports = function (app,io){
     })
 
     app.get('/api/games/favorites', function(req,res){
-        db.Vote.findAll({
+        db.User.findOne({
             where: {
-                UserId: req.user.id,
-                upDown: true,
-                GameId: {$not: null}
-            },
-            include: [db.Game]
+                UserId: req.user.id},
+                include: [db.Game]
         }).then((votes) => {
             res.json(votes)
         })
--- seeds.sql
+++ seeds.sql
@@ -1,23 +1,64 @@
 USE VG_Nexus_db;
 
-INSERT INTO Posts (text, ThreadId, UserId, createdAt, updatedAt) VALUES
-("Man, I'm sorry, I honestly can't think of any announcements at this point. We'll see...", 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("bump", 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("dude, don't bump announcement threads.", 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- INSERT INTO Users (username, email, password, postBanner, bio, createdAt, updatedAt) VALUES
+-- ("holfman", "haffedmcnair@test.com", "test", "pretty picture", "just a fake dude", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("bootlicker", "bootsntoots@test.com", "heel", "ugly picture", "A boot maker", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("tacobell", "causesacidreflux@darknet.net", "hacked", "picture of burrito", "forgotten bio", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-("I made a game! blah blah blah blah blah...", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("I cannot believe how unprofessional this website is. I loaded it up and now my computer has a million viruses on1 n23i it i 1234c acant 2324234halp34234halp please1231111", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 3, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("excuse me?", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("I made a game! blah blah blah blah blah...", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("I cannot believe how unprofessional this website is. I loaded it up and now my computer has a million viruses on1 n23i it i 1234c acant 2324234halp34234halp please1231111", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("nice", 3, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("excuse me?", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("Where can you get gas for $1.49?", 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("where", 4, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("taco bell", 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-("lol", 4, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+-- INSERT INTO Games (name, description, UserId, createdAt, updatedAt) VALUES
+-- ("Sonic", 'Gotto go fast', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ('Pokemon', 'Catch em all', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ('Runescape', 'hd new age game', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+
+INSERT INTO Tags (name,  createdAt, updatedAt) VALUES
+("Action", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+("Arcade", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+("Shooter", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+("Role Playing", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+
+-- INSERT INTO Forums (title, description, createdAt, updatedAt) VALUES
+-- ("General", "The place to discuss anything and everything about this site", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Games", "For discussing specific games", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Technical issues", "If it's broken, let's fix it: game help, site bugs and feedback", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Free for all", "The place for off-topic threads", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Forum rules/FAQ", "Beginners, look here", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+-- INSERT INTO Threads (title, pinned, ForumId, UserId, createdAt, updatedAt) VALUES
+-- ("Website Announcements", true, 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("My first game thread", false, 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("This site is breaking my computer", false, 3, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Jokes megathread", true, 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Posting rules (Sticky)", true, 5, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+-- INSERT INTO Posts (text, ThreadId, UserId, createdAt, updatedAt) VALUES
+-- ("Man, I'm sorry, I honestly can't think of any announcements at this point. We'll see...", 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("bump", 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("dude, don't bump announcement threads.", 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("I made a game! blah blah blah blah blah...", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("nice", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("nice", 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("I cannot believe how unprofessional this website is. I loaded it up and now my computer has a million viruses on1 n23i it i 1234c acant 2324234halp34234halp please1231111", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("nice", 3, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("excuse me?", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("I made a game! blah blah blah blah blah...", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("nice", 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("nice", 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("I cannot believe how unprofessional this website is. I loaded it up and now my computer has a million viruses on1 n23i it i 1234c acant 2324234halp34234halp please1231111", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("nice", 3, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("excuse me?", 3, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Where can you get gas for $1.49?", 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("where", 4, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("taco bell", 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("lol", 4, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Rule #1: don't talk aobut VG Nexus. Rule #2: a cheeseburger a day keeps the doctor away. Rule #3: guess I'll just die then?", 5, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("absolute shitpost", 5, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("nice", 5, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+-- ("gotta go fast boys", 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("STOP TALKING ABOUT SONIC. NO ONE GETS IT", 4, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("so like, does anyone actually read these posts?", 4, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("don't break the fourth wall guys.", 4, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("Rule #1: don't talk aobut VG Nexus. Rule #2: a cheeseburger a day keeps the doctor away. Rule #3: guess I'll just die then?", 5, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("absolute shitpost", 5, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+-- ("nice", 5, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
